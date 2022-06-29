a7a3cfc731e67e99819ddba9ef4a2505ff430034
JAY KIM <jaykim@JAYs-MBP-2018.senecacollege.ca>
2022-06-29 15:02:36 -0400

    updated data type

--- dummy-data/EventData.js
+++ dummy-data/EventData.js
@@ -3,8 +3,8 @@ const EventData = [
     id: 1,
     title: 'Make a friend to my dog',
     date: '3 July, 2022',
-    // Array of Park Id
-    parkLocation: [1],
+    // Park Id
+    parkLocation: 1,
     // Array of User Id
     users: [1],
   },
