4dbdbe4c0b5cdcc4d444b692c41561ec29d3b361
JAY KIM <jaykim@JAYs-MBP-2018.senecacollege.ca>
2022-07-05 13:01:37 -0400

    Updated init Data

--- src/context/HomeViewer.js
+++ src/context/HomeViewer.js
@@ -1,8 +1,9 @@
 import React, { createContext, useEffect, useMemo, useState } from 'react';
+import { GOOGLE_MAPS_APIKEY } from '@env';
 
 import UserData from '../../dummy-data/UserData';
 import EventData from '../../dummy-data/EventData';
-import ParkData from '../../dummy-data/ParkData';
+import { normalizeParkList, fetchParkList } from '../../dummy-data/ParkData';
 import ReviewData from '../../dummy-data/ReviewData';
 
 export const HomeViewerContext = createContext({
@@ -16,24 +17,32 @@ export const HomeViewerContext = createContext({
 
 function HomeViewerContextProvider({ children }) {
   const [viewer, setViewer] = useState({
-    UserData: [],
-    EventData: [],
+    UserData,
+    EventData,
     ParkData: [],
-    ReviewData: [],
-    LoginUser: {},
+    ReviewData,
+    LoginUser: UserData[0],
     SearchedData: [],
   });
+  const [filteredParkList, setFilteredParkList] = useState([]);
 
   useEffect(() => {
-    setViewer({
-      UserData,
-      EventData,
-      ParkData,
-      ReviewData,
-      LoginUser: UserData[0],
-      SearchedData: [],
-    });
-  }, []);
+    filterParkList();
+
+    if (filteredParkList.length > 0) {
+      setViewer({
+        ...viewer,
+        ParkData: filteredParkList,
+      });
+    }
+  }, [filteredParkList]);
+
+  const filterParkList = async () => {
+    const res = await fetchParkList();
+    const fetched = await res.json();
+
+    setFilteredParkList(normalizeParkList(fetched.results, GOOGLE_MAPS_APIKEY));
+  };
 
   const value = useMemo(
     () => ({
